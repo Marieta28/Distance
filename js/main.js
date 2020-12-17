fb10c33f6052a9a3fd60e4c8a3f999bd11e933db
Marieta28 <69346003+Marieta28@users.noreply.github.com>
2020-12-18 01:19:15 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,5 @@
+const manuBtn = document.querySelector(".m-menu")
+const manu = document.querySelector(".menu-sidebar")
+manuBtn.addEventListener('click', function(){
+  manu.classList.toggle("active")
+})
